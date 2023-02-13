66116833abe7c4bb3a7bb8c9515ca49139da6432
Alex Fraga <125224356+Fragale013@users.noreply.github.com>
2023-02-12 21:08:36 -0300

    Create DisparoWppDestinos.py

---
+++ DisparoWppDestinos.py
@@ -0,0 +1,13 @@
+## esse arquivo irá definir os destinos e quais arquivos de imagem serão enviados
+#destinos devem estar adicionados na lista de contatos da conta whatsapp que realizará o envio
+#É necessário apontar o caminho completo até a imagem
+#imagens devem estar em formato jpg, porém na hora de definir a imagem não é necessário informar o formato
+#exemplo João recebe imagem1.jpg = ('João','imagem1)
+
+
+lista_envio = [
+    ('Grupo Administradores', 'C:/Imagens/Relatorios/Cenario_Brasil'),
+    ('Gerente SUL', 'C:/Imagens/Relatorios/Cenario_Sul'),
+    ('Gerente NORTE', 'C:/Imagens/Relatorios/Cenario_NORTE'),
+    ('Gerente LESTE', 'C:/Imagens/Relatorios/Cenario_LESTE')
+    ]
